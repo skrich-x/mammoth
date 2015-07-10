cabcab204ac395952062dae600ec9663830787bf
Kyle Richardson <s.k.richardson7@gmail.com>
2015-07-10 08:45:39 -0400

    Create Task Button Console Logs _ Action recognized properly
    Small styling changes began

--- app/routes/tasks/create.js
+++ app/routes/tasks/create.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  actions: {
+    createTask: function(){
+      // user.set('task', user.get('task'));
+      console.log('create Task button');
+
+    }
+  }
 });
