1b2e48d8886daf28ba48f606be518b4c6b802816
Kyle Richardson <s.k.richardson7@gmail.com>
2015-07-19 21:49:04 -0400

    put header into its own component

--- app/components/full-calendar.js
+++ app/components/full-calendar.js
@@ -5,19 +5,21 @@ export default Ember.Component.extend({
   _initializeCalendar: (function() {
     return $("#calendar").fullCalendar({
       googleCalendarApiKey: 'AIzaSyB6-NLaDa-cA5XhaKPDp2mHKUhnk7L0cBI',
-              events: {
-                  googleCalendarId: 'tiymammoth@gmail.com'
-              },
+              events: [{
+                  googleCalendarId: 'tiymammoth@gmail.com',
+                  title: "newEvent",
+                  start:'2015-07-24T05:00:00'
+                }],
+                   selectable: true,
+                   editable: true,
+                   header: {
+                           left: 'prev,next today',
+                           center: 'title',
+                           right: 'month,agendaWeek,agendaDay'
+                         }
+
       // events: this.theEvents
     });
   }).on("didInsertElement")
-});
 
-  // gCal: function(){
-  //     $('#calendar').fullCalendar({
-  //         googleCalendarApiKey: 'AIzaSyB6-NLaDa-cA5XhaKPDp2mHKUhnk7L0cBI',
-  //         events: {
-  //             googleCalendarId: 'tiymammoth@gmail.com'
-  //         }
-  //     });
-  //   }
+});
--- app/components/tasks-create.js
+++ app/components/tasks-create.js
@@ -5,11 +5,37 @@ export default Ember.Component.extend({
   actions: {
     createTask: function() {
       this.sendAction('action', this.get('model'));
+      var events=[];
+      this.events.pushObject(newEvent);
+      this.$("#calendar").fullCalendar('renderEvent', newEvent, true);
+
     },
 
     invalidateSession: function() {
       this.get('session').invalidate();
     },
 
+  //
+  //   addEvent: function() {
+  //   var newEvent = [];
+  //   this.events.pushObject(newEvent);
+  //   this.$("#calendar").fullCalendar('renderEvent', newEvent, true);
+  // }
+
   }
 });
+//
+// $('#calendar').fullCalendar({
+//     events: function(start, end, timezone, callback) {
+//                 var events = [];
+//                 $(doc).find('event').each(function() {
+//                     events.push({
+//                         title: $(this).attr('title'),
+//                         start: $(this).attr('start') // will be parsed
+//                     });
+//                 });
+//                 callback(events);
+//
+//         });
+//     }
+// });
