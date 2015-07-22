2dbe3f2bb83c3fe1dc142ed7239458de66179ef1
Kyle Richardson <s.k.richardson7@gmail.com>
2015-07-22 08:41:50 -0400

    Hooking up destroyTask

--- app/components/tasks-manage.js
+++ app/components/tasks-manage.js
@@ -2,12 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  actions:{
-
-    removeSubtask: function(subtaskObject){
-      var subtaskArray = this.get('model.subtaskArray');
-      subtaskArray.removeObject(subtaskObject);
-      this.sendAction('save');
-    },
-  }
+  	actions: {
+  		destroyTask: function(task){
+  			this.sendAction('action', task);
+  		}
+  	}
 });
